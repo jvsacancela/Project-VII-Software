5627f27c9b80415457f69b9098d9976b7bab8747
Javier Sacancela <javiersacancela@gmail.com>
2021-02-11 23:18:10 -0500

    se modifico la estructura de las carpetas PAGES

---
+++ config/conexion.php
@@ -0,0 +1,11 @@
+<?php 
+
+    define ('SERVIDOR', 'localhost');
+    define ('USUARIO', 'root');
+    define ('CLAVE', '');
+    define ('BDD', 'importadorabd');
+
+    $conexion = mysqli_connect(SERVIDOR, USUARIO, CLAVE, DBB) or die ('Error :c);
+
+
+?>
--- src/nav/footer.php
+++ src/nav/footer.php
@@ -1,7 +1,20 @@
-<footer>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, sit nesciunt. Laborum, ipsum ex. Ex doloribus porro asperiores necessitatibus voluptates quo iusto magni mollitia nihil impedit. Deleniti, cum optio eveniet nisi corporis repellendus, quas perspiciatis delectus commodi voluptate a, incidunt doloremque ipsum cumque aliquid eaque beatae iure quos non doloribus aut repellat praesentium repudiandae ullam? Voluptatum, nisi! Id, obcaecati blanditiis facilis dignissimos voluptatum ex, nostrum esse velit corporis non, dolor molestiae ipsam veniam aliquid a veritatis repellat earum. Facere reprehenderit, officia voluptatum obcaecati reiciendis optio expedita cum culpa? Mollitia accusamus rem rerum sequi sint culpa tempore iste natus dolores voluptates.</p>
+<footer id="footer-index">
+    <div id="footer-enlaces">
+        <div>
+            <p> <a id="enlace-fot" href="">Conocenos</a> </p>
+        </div>
+
+        <div>
+            <p> <a id="enlace-fot" href="">Como comprar?</a> </p>
+        </div>
+
+        <div>
+            <p> <a id="enlace-fot" href="src/pages/info/page-contacto.php">Para negocio</a> </p>
+        </div>
+    </div>
+    
     <br>
     <hr>
     <br>
-    <p>Hello 😊</p>
+    <p>SOMOS IMPORTADORA MENDEZ - IMPORTADORES POR EXCELENCIA</p>
 </footer>
