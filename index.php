87567b817c065ffc103b6529b9e70869cce6726c
Javier Sacancela <javiersacancela@gmail.com>
2021-02-11 20:05:15 -0500

    inicio del proyecto

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMPORTADORA MENDEZ | IMPORTADORES POR EXCELENCIA</title>
+    <!--estilos-->
+    <link rel="stylesheet" href="src/style/general.css">
+    <link rel="stylesheet" href="src/nav/style/menu.css">
+    <link rel="stylesheet" href="src/nav/style/footer.css">
+    <link rel="stylesheet" href="src/style/index.css">
+</head>
+<body>
+    <!--navegacion-->
+    <?php
+    require_once 'src/nav/menu.php'
+    ?>
+    <br>
+
+    <div id="contenedor">
+        <h1>SOMOS IMPORTADORA MENDEZ</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus hic officia corporis perspiciatis voluptatum odio quidem voluptate voluptas at quae. Est quod id tenetur sapiente a tempore asperiores deleniti enim consequuntur inventore odit nulla quasi, molestias iusto neque maxime dolores, illo, repudiandae eos rerum aspernatur? Incidunt expedita minus voluptatem, dicta eum necessitatibus accusantium ea numquam ipsam, corporis, impedit quae sequi ad sint. Velit, a in, qui asperiores ipsam eos nisi fuga dolore cumque doloremque ipsum nemo. Magni nam quo qui, inventore perferendis aut animi totam delectus dolores libero ab minima sapiente blanditiis, magnam nihil, incidunt impedit commodi alias. Quos laudantium consequatur vero non unde sint fugit assumenda maxime quasi? Distinctio sapiente cum quidem, delectus explicabo velit nihil repellat suscipit maxime adipisci eos dolorem? Eum eaque ipsam reiciendis veritatis commodi soluta iure, dignissimos repudiandae labore consequuntur, eius ducimus cumque excepturi aspernatur doloribus tenetur praesentium impedit ipsa perferendis minus voluptatum quis? Cumque!</p>
+    </div>
+    
+    <br>
+    <!--footer-->
+    <?php
+    require_once 'src/nav/footer.php'
+    ?>
+
+    <script src="src/nav/menu.js"></script>
+</body>
+</html>
---
+++ src/nav/menu.php
@@ -0,0 +1,63 @@
+<nav id="navegacion">
+    <div id="nav-div">
+        <div id="nav-marca">
+            <a id="enlace-menu"href=""><img id="img-logo"src="img/logo.jpg" alt="logo"></a>
+        </div>
+       
+        <div id="nav-buscar">
+            <input id="btn-filtro" type="button" value="Filtro">
+            <input id="input-buscar" type="text">
+            <input id="btn-buscar" type="button" value="Buscar">
+        </div>
+
+        <div id="nav-compras">
+        <p>Carrito de compras aqui</p>
+        </div>
+           
+    </div>
+
+    <div id="nav-enlaces">
+        <input type="checkbox" name="checkbox-categoria" id="cb-categoria" class="input-check">
+        <label id="btn-menu" for="cb-categoria"></label>
+        
+        <!--categorias-->
+        <div id="content-categorias" class="content">
+            <div>
+                <p><a id="enlace-categoria" href="">Accesorios</a> </p>
+                <p> <a id="enlace-categoria" href="">Maquillaje</a> </p>
+                <p> <a id="enlace-categoria" href="">Labiales</a> </p>
+                <p> <a id="enlace-categoria" href="">Esponjas</a> </p>
+                <p> <a id="enlace-categoria" href="">Uñitas</a> </p>
+                <p> <a id="enlace-categoria" href="">Diademas</a> </p>
+            </div>
+
+            <div>
+                <p> <a id="enlace-categoria" href="">Cosmeticos</a> </p>
+                <p> <a id="enlace-categoria" href="">Splash</a> </p>
+                <p> <a id="enlace-categoria" href="">Sombras</a> </p>
+                <p> <a id="enlace-categoria" href="">Acrilico</a> </p>
+                <p> <a id="enlace-categoria" href="">Juguetes</a> </p>
+                <p> <a id="enlace-categoria" href="">Cintillos</a> </p>
+            </div>
+
+            <div>
+                <p> <a id="enlace-categoria" href="">Aretes</a> </p>
+                <p> <a id="enlace-categoria" href="">Facial</a> </p>
+                <p> <a id="enlace-categoria" href="">Fiesta</a> </p>
+                <p> <a id="enlace-categoria" href="">Stickers</a> </p>
+                <p> <a id="enlace-categoria" href="">Pestañas</a> </p>
+                <p> <a id="enlace-categoria" href="">Capilar</a> </p>
+            </div>
+
+            <div>
+                <p> <a id="enlace-categoria" href="">Moños</a> </p>
+                <p> <a id="enlace-categoria" href="">Esmaltes</a> </p>
+                <p> <a id="enlace-categoria" href="">Brochas</a> </p>
+                <p> <a id="enlace-categoria" href="">Monederos</a> </p>
+                <p> <a id="enlace-categoria" href="">Relojes</a> </p>
+                <p> <a id="enlace-categoria" href="">Gorros</a> </p>
+            </div>
+            
+        </div>
+    </div> 
+</nav>
