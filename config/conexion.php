17ebe9329de9c14b5e8e6883f1dff8f9eca7e6a4
Javier Sacancela <68529184+jvsacancela@users.noreply.github.com>
2021-03-17 00:47:05 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
     define ('SERVIDOR', 'localhost');
-    define ('USUARIO', 'u614831242_impmendez99');
-    define ('CLAVE', 'Unapapay1cola');
+    define ('USUARIO', 'u614831242_impmendez');
+    define ('CLAVE', 'Dospapasy1cola');
     define ('BDD', 'u614831242_impmendezbd');
 
     $conexion = mysqli_connect(SERVIDOR, USUARIO, CLAVE, BDD) or die ('Error :c');
